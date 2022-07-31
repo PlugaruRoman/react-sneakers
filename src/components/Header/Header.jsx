06db2716cf21b3044f431faf0ee8e71a13a657e6
PlugaruRoman <97292989+PlugaruRoman@users.noreply.github.com>
2022-07-31 20:33:37 +0300

    test1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
 
   return (
     <header className={styles.header}>
-      <Link to={'https://plugaruroman.github.io/react-sneakers/'}>
+      <Link to={'/react-sneakers/'}>
         <div className={styles.headerLeft}>
           <img witdh={40} height={40} src='img/logo.png' alt='logo' />
           <div className={styles.columnInfo}>
@@ -52,7 +52,7 @@ const Header = (props) => {
           </svg>
           <span>{sumPrice} руб.</span>
         </li>
-        <Link to={'https://plugaruroman.github.io/react-sneakers/favorites'}>
+        <Link to={'/react-sneakers/favorites'}>
           <li className={styles.navLink}>
             <svg
               width='20'
@@ -69,7 +69,7 @@ const Header = (props) => {
           </li>
         </Link>
 
-        <Link to={'https://plugaruroman.github.io/react-sneakers/orders'}>
+        <Link to={'react-sneakers/orders'}>
           <li className={styles.navLink}>
             <svg
               width='20'
